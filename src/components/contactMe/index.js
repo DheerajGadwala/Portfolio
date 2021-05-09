233b467ea1ea019478122409dfd23a283ae25232
Dheeraj Gadwala <drgad24@gmail.com>
2021-05-10 00:21:14 +0530

    used axios to make send email api calls from emailjs

--- src/components/contactMe/index.js
+++ src/components/contactMe/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import validator from 'validator';
-import {SMTPClient} from 'emailjs';
+import axios from 'axios';
 import linkedIn from './images/linkedIn.png';
 import github from  './images/github.png';
 import './style.css';
@@ -37,10 +37,23 @@ const ContactMe = ()=>{
             }
             if(message.value===""){
                 setMessageError("Message can't be empty");
-                message.classList.add('errorTextArea');
+                message.classList.add('errorInputs');
                 document.querySelector('#message + label').classList.add('errorLabel');
             }
             if(nameError==="" && emailError==="" && messageError===""){
+
+                //used axios to make api call to emailjs-com
+                var form = document.querySelector('.infoForm');
+                var data = new FormData(form);
+                data.append('service_id', 'gmail');
+                data.append('template_id', 'portfolioTemplate');
+                data.append('user_id', 'user_1x2mJlFIf0vuZZg7O67G0');
+                //console.log(data);
+                axios({
+                    method: 'post',
+                    url: 'https://api.emailjs.com/api/v1.0/email/send-form',
+                    data: data,
+                }).then(res=>console.log(res));
 
             } 
         });
@@ -61,7 +74,7 @@ const ContactMe = ()=>{
                 }
                 else if(item.id==="message"){
                     setMessageError("");
-                    document.getElementById('message').classList.remove('errorTextArea');
+                    document.getElementById('message').classList.remove('errorInputs');
                     document.querySelector('#message + label').classList.remove('errorLabel');
                 }    
             });
@@ -75,26 +88,26 @@ const ContactMe = ()=>{
                     Wanna reach out to me?
                 </h2>
             </div>
-            <div className="infoForm">
+            <form className="infoForm">
                 <div className="inputText">
-                    <input type="text" required id="name"/>
+                    <input type="text" required id="name" name="name"/>
                     <label className="inputTextLabel"><span>Name*</span></label>
                     <span className="error">{nameError}</span>
                 </div>
                 <div className="inputText">
-                    <input type="text" required id="email"/>
+                    <input type="text" required id="email" name="email"/>
                     <label className="inputTextLabel"><span>Email*</span></label>
                     <span className="error">{emailError}</span>
                 </div>
                 <div className="inputTextArea">
-                    <textarea required id="message"/>
+                    <textarea required id="message" name="message"/>
                     <label className="inputTextAreaLabel"><span>Message*</span></label>
                     <span className="error">{messageError}</span>
                 </div>
                 <div className="sendButton">
                     SEND
                 </div>
-            </div>
+            </form>
             <div className="contactMeIcons">
                 <a href="https://github.com/DheerajGadwala"><img src={github}/></a>
                 <a href="https://www.linkedin.com/in/dheerajgadwala/"><img src={linkedIn}/></a>
